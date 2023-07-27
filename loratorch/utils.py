2343c25664c05cce788a9097900053636cb600f4
Baijiong Lin <bj.lin.email@gmail.com>
2023-07-28 00:40:18 +0800

    update

--- loratorch/utils.py
+++ loratorch/utils.py
@@ -1,7 +1,4 @@
 #  ------------------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation. All rights reserved.
-#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-#
 #  This code is reconstructed based on loralib (https://github.com/microsoft/LoRA) by Baijiong Lin.
 #  ------------------------------------------------------------------------------------------
 import torch
